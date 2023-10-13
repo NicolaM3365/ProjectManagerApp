6e0e0c914b1da4db29822ac1f376c2560699408b
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2023-10-13 18:47:10 +0100

    statusFilter

--- static/script.js
+++ static/script.js
@@ -1,24 +1,28 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
     // Get reference to the dropdown filter and table rows
     const statusFilter = document.getElementById('statusFilter');
     const tableRows = document.querySelectorAll("#taskTable tbody tr");
 
-    // Attach event listener to the dropdown filter
-    statusFilter.addEventListener("change", function() {
-        const selectedStatus = statusFilter.value;
+    if (statusFilter) {
+        // Attach event listener to the dropdown filter
+        statusFilter.addEventListener("change", function() {
+            const selectedStatus = statusFilter.value;
 
-        tableRows.forEach(function(row) {
-            const statusCell = row.querySelector(".task-status");
-            if (!selectedStatus || statusCell.innerText === selectedStatus) {
-                row.style.display = ""; // show the row
-            } else {
-                row.style.display = "none"; // hide the row
-            }
+            tableRows.forEach(function(row) {
+                const statusCell = row.querySelector(".task-status");
+                if (!selectedStatus || statusCell.innerText === selectedStatus) {
+                    row.style.display = ""; // show the row
+                } else {
+                    row.style.display = "none"; // hide the row
+                }
+            });
         });
-    });
+    } else {
+        console.error("Element with ID 'statusFilter' not found!");
+    }
 });
+
 
 
 
