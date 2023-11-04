98f4ff8ae7a3702293afc9eaa3999da2a933a30e
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2023-11-04 17:41:38 +0000

    virtual env

--- models.py
+++ models.py
@@ -24,6 +24,7 @@ The owner of a project is a user."""
 
 #TO DO check db.Column versus db.mapped_column
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'  # specify the table name explicitly
     id = db.mapped_column(db.Integer, primary_key=True)
     username = db.mapped_column(db.String(50), unique=True)
     # SECURITY NOTE: Don't actually store passwords like this in a real system!
@@ -36,16 +37,18 @@ class User(UserMixin, db.Model):
 
 
 class Task(db.Model):
+        __tablename__ = 'tasks'  # specify the table name explicitly
         task_id = db.mapped_column(db.Integer, primary_key=True)
         name = db.mapped_column(db.String(100), nullable=False)
         description = db.mapped_column(db.String(100), nullable=False)
         status = db.mapped_column(db.String(100), nullable=False)
         # assigned_to = db.mapped_column(db.String(100), nullable=False)
         created_at = db.mapped_column(db.DateTime, default=datetime.utcnow)
-        project_id = db.mapped_column(db.Integer, db.ForeignKey("project.project_id"), nullable=False)
+        project_id = db.mapped_column(db.Integer, db.ForeignKey("projects.project_id"), nullable=False)
         project = db.relationship('Project', backref=db.backref('tasks', lazy=True))
-        managed_task_id = db.mapped_column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        managed_task_id = db.mapped_column(db.Integer, db.ForeignKey('users.id'), nullable=False)
         managed_task = db.relationship('User', backref=db.backref('tasks', lazy=True))
+        
         
         
         def __str__(self):
@@ -53,12 +56,13 @@ class Task(db.Model):
         
 
 class Project(db.Model):
+        __tablename__ = 'projects'  # specify the table name explicitly
         project_id = db.mapped_column(db.Integer, primary_key=True) 
         name = db.mapped_column(db.String(100), nullable=False)
         description = db.mapped_column(db.String(100), nullable=False)
         status = db.mapped_column(db.String(100), nullable=False)
         created_at = db.mapped_column(db.DateTime, default=datetime.utcnow)
-        managed_project_id = db.mapped_column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        managed_project_id = db.mapped_column(db.Integer, db.ForeignKey('users.id'), nullable=False)
         managed_project = db.relationship('User', backref=db.backref('projects', lazy=True))
 
         def add_task(self, task):
